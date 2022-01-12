aa0334a4a2f67994db31d730e852b241dc4440d0
Shailesh Singh <shailesh@mit.tc>
2022-01-12 19:16:45 +0530

    Time: 4260 ms (72.04%), Space: 173.1 MB (53.76%) - LeetHub

--- 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.py
+++ 1916-count-ways-to-build-rooms-in-an-ant-colony/1916-count-ways-to-build-rooms-in-an-ant-colony.py
@@ -1,17 +1,18 @@
 class Solution:
-    def waysToBuildRooms(self, prevRoom: List[int]) -> int:
-        g = defaultdict(list)
-        for i in range(1, len(prevRoom)):
-            g[prevRoom[i]].append(i)
-        mod = 10 ** 9 + 7
-
-        def dfs(i):
-            number_of_edges = 0
-            res = 1
-            for j in g[i]:
-                number_of_child_edges, number_of_perm = dfs(j)
-                number_of_edges += number_of_child_edges
-                res = (res * comb(number_of_edges, number_of_child_edges) * number_of_perm) % mod
-            return (number_of_edges + 1, res)
-
-        return dfs(0)[1]
+    def waysToBuildRooms(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        g = collections.defaultdict(list)
+        for cur, pre in enumerate(arr):
+            g[pre].append(cur)
+            
+        def dfs(cur):
+            if not g[cur]:
+                return 1, 1
+            ans, l = 1, 0
+            for nxt in g[cur]:
+                tmp, r = dfs(nxt)
+                ans = (ans * tmp * math.comb(l+r, r)) % MOD
+                l += r
+            return ans, l + 1
+            
+        return dfs(0)[0]
